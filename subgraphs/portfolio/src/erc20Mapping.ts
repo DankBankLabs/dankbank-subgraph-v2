46edb96b65a05f9df2f07087c19acd6f008d5097
christian-miljkovic <christian.miljkovic@gmail.com>
2022-02-16 17:06:32 -0700

    wip: handleTransfer mapping

--- subgraphs/portfolio/src/erc20Mapping.ts
+++ subgraphs/portfolio/src/erc20Mapping.ts
@@ -1,3 +1,6 @@
+import { bigZero } from "../../utils/constants";
+import { AddressZero } from "../../utils/constants";
+
 function getTokenBalance(tokenAddress: string, user: string): TokenBalance {
   let balanceId = tokenAddress + user;
   let tokenBalance = TokenBalance.load(balanceId);
@@ -15,23 +18,18 @@ export function handleTransfer(event: Transfer): void {
   let tokenAddress = event.address.toHexString();
   let value = event.params.value;
 
-  let tokenVault = getTokenVault(tokenAddress);
-
   // add value to to
   let toAddress = event.params.to.toHexString();
 
   // when burning a token the to Address is the 0 address in the event
   if (toAddress != AddressZero) {
-    ensureUserCreated(toAddress);
+    ensureAccountCreated(toAddress);
 
     let toBalance = getTokenBalance(tokenAddress, toAddress);
 
     toBalance.balance = toBalance.balance.plus(value);
 
     toBalance.save();
-  } else {
-    // tokens were burned
-    tokenVault.totalSupply = tokenVault.totalSupply.minus(value);
   }
 
   let fromAddress = event.params.from.toHexString();
@@ -45,14 +43,5 @@ export function handleTransfer(event: Transfer): void {
     fromBalance.balance = fromBalance.balance.minus(value);
 
     fromBalance.save();
-  } else {
-    // the tokens were minted if they were from the 0 address
-    tokenVault.totalSupply = tokenVault.totalSupply.plus(value);
   }
-
-  // update reserve price
-  let tokenVaultContract = ERC721TokenVault.bind(event.address);
-  tokenVault.reservePrice = tokenVaultContract.reservePrice();
-
-  tokenVault.save();
 }
---
+++ subgraphs/portfolio/src/utils/constants.ts
@@ -0,0 +1,7 @@
+import { BigInt, TypedMap } from "@graphprotocol/graph-ts";
+
+export const bigZero = BigInt.fromI32(0);
+export const bigOne = BigInt.fromI32(1);
+export const weiPerEth = BigInt.fromI32(10).pow(18);
+
+export const AddressZero = "0x0000000000000000000000000000000000000000";
