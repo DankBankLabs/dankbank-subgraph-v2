4093b7dd013dba4a707265f560f7e1794d5dfe6b
christian-miljkovic <christian.miljkovic@gmail.com>
2022-02-17 20:59:08 -0700

    fix: importing events

--- subgraphs/portfolio/src/erc20Mapping.ts
+++ subgraphs/portfolio/src/erc20Mapping.ts
@@ -1,5 +1,39 @@
-import { bigZero } from "../../utils/constants";
-import { AddressZero } from "../../utils/constants";
+import { AddressZero, bigZero } from "./utils/constants";
+import { ensureAccountCreated } from "./utils/accounts";
+import { Transfer } from "../generated/10GuyERC20/ERC20";
+import { TokenBalance } from "../generated/schema";
+
+export function handleTransfer(event: Transfer): void {
+  const tokenAddress = event.address.toHexString();
+  const value = event.params.value;
+
+  // add value to to
+  const toAddress = event.params.to.toHexString();
+
+  // when burning a token the to Address is the 0 address in the event
+  if (toAddress != AddressZero) {
+    ensureAccountCreated(toAddress);
+
+    const toBalance = getTokenBalance(tokenAddress, toAddress);
+
+    toBalance.balance = toBalance.balance.plus(value);
+
+    toBalance.save();
+  }
+
+  const fromAddress = event.params.from.toHexString();
+
+  // when minting a token the from address is the 0 address in the event
+  if (fromAddress != AddressZero) {
+    ensureAccountCreated(fromAddress);
+
+    const fromBalance = getTokenBalance(tokenAddress, fromAddress);
+
+    fromBalance.balance = fromBalance.balance.minus(value);
+
+    fromBalance.save();
+  }
+}
 
 function getTokenBalance(tokenAddress: string, user: string): TokenBalance {
   const balanceId = tokenAddress + user;
@@ -12,36 +46,4 @@ function getTokenBalance(tokenAddress: string, user: string): TokenBalance {
   }
 
   return tokenBalance as TokenBalance;
-}
-
-export function handleTransfer(event: Transfer): void {
-  let tokenAddress = event.address.toHexString();
-  let value = event.params.value;
-
-  // add value to to
-  let toAddress = event.params.to.toHexString();
-
-  // when burning a token the to Address is the 0 address in the event
-  if (toAddress != AddressZero) {
-    ensureAccountCreated(toAddress);
-
-    let toBalance = getTokenBalance(tokenAddress, toAddress);
-
-    toBalance.balance = toBalance.balance.plus(value);
-
-    toBalance.save();
-  }
-
-  let fromAddress = event.params.from.toHexString();
-
-  // when minting a token the from address is the 0 address in the event
-  if (fromAddress != AddressZero) {
-    ensureUserCreated(fromAddress);
-
-    let fromBalance = getTokenBalance(tokenAddress, fromAddress);
-
-    fromBalance.balance = fromBalance.balance.minus(value);
-
-    fromBalance.save();
-  }
 }
