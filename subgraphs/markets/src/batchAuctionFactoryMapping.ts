8db6bfadc96b9313b345905343d4e4c61f7bb15e
Jack Gerulskis <jfgerulskis@wpi.edu>
2022-02-20 19:43:26 -0500

    first working markets build on main, wip

---
+++ subgraphs/markets/src/batchAuctionFactoryMapping.ts
@@ -0,0 +1,19 @@
+
+import { MarketCreated } from "../generated/BatchAuctionFactory/MISOMarket";
+import { BatchAuction } from "../generated/templates";
+
+import { bigOne } from "./utils/constants";
+import { createAuction, loadOrCreateMisoMarket } from "./utils/miso";
+
+
+export function handleCreateMarket(event: MarketCreated): void {
+  BatchAuction.create(event.params.addr);
+
+  // increment number of auction
+  let misoMarket = loadOrCreateMisoMarket(event.address.toHexString());
+  misoMarket.numberOfAuctions = misoMarket.numberOfAuctions.plus(bigOne);
+  misoMarket.save();
+
+  // create Auction entity
+  createAuction(event.params.addr, misoMarket.id).save();
+}
