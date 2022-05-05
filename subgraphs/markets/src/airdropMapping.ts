baba4199eef0ed66475be561d6ee4eb7eba9f67f
crossxcell99 <crossxcell99@gmail.com>
2022-05-05 15:05:44 +0100

    feat: add txn for airdrop claim

---
+++ subgraphs/markets/src/airdropMapping.ts
@@ -0,0 +1,24 @@
+import { MerkleClaimERC20, Claim } from "../generated/templates/MerkleClaimERC20/MerkleClaimERC20";
+import { TransactionType } from "./utils/constants";
+import { getLiquidityPool } from "./utils/market";
+import { createTransaction } from "./utils/transaction";
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export function handleClaim(event: Claim): void {
+  let contract = MerkleClaimERC20.bind(event.address);
+
+  let pool = getLiquidityPool(
+    contract.memeTokenAddress().toHexString(),
+    event.block.timestamp
+  );
+
+  createTransaction(
+    event.transaction.hash.toHex(),
+    event.params.to.toHexString(),
+    pool.id,
+    BigInt.fromI32(0), // gotten for free
+    [event.params.amount],
+    TransactionType.CLAIM_FROM_AIRDROP,
+    event.block.timestamp,
+  ).save();
+}
